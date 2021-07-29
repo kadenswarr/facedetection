2b2fc46dc53505a0d0c6a35205ba7469acb09a88
kaden <none@none.com>
2021-07-28 22:12:37 -0600

    fixes

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends React.Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch("https://rocky-harbor-79932.herokuapp.com:3000/image", {
+        fetch("https://rocky-harbor-79932.herokuapp.com/image", {
           method: "put",
           headers: {"content-type": "application/json"},
           body: JSON.stringify({
